4e126b0591b34058432f7faa42d9075140df2f5c
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-21 13:57:37 -0600

    step 1

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+export default class TopicBrowser extends Component {
+    render () {
+        return (
+            <div>
+                <EvenAndOdd />
+                <FilterObject />
+                <FilterString />
+                <Palindrome />
+                <Sum />
+            </div>
+        )
+    }
+}
+
+
+
