14fa8c63958019e55fdc2587fb8186013c3846c4
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-21 15:28:58 -0600

    just going with what yall have I cant find the err

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 export default class EvenAndOdd extends Component {
 
-constructor () {
+  constructor() {
     super();
 
     this.state ={
-        evenArray: [],
-        oddArray: [],
-        userInput: ' '
+      evenArray: [],
+      oddArray: [],
+      userInput: ''
     }
-}
+  }
 
-handleChange(val) {
+  handleChange(val) {
     this.setState({ userInput: val });
-}
+  }
 
-assignEvenAndOdds(userInput) {
+  assignEvenAndOdds(userInput) {
     let arr = userInput.split(',');
     let evens = [];
     let odds = [];
@@ -30,20 +30,17 @@ assignEvenAndOdds(userInput) {
     }
 
     this.setState({ evenArray: evens, oddArray: odds });
-}
+  }
 
-
-
-    render() {
-        return (
-            <div className="puzzleBox evenAndOddPB">
-                <h4>Evens and Odds</h4>
-                <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value)} />
-                <button className="confirmationButton" onClick={ () => { this.assignEvenAndOdds (this.userInput)}} > Split </button>
-                <span className="resultBox" > Even: {JSON.stringify(this.state.evenArray) } </span>
-                <span className="resultBox" > Odd: {JSON.stringify(this.state.oddsArray) } </span>  
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div className="puzzleBox evenAndOddPB">
+        <h4> Evens and Odds </h4>
+        <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }/>
+        <button className="confirmationButton" onClick={ () => { this.assignEvenAndOdds(this.state.userInput) }}> Split </button>
+        <span className="resultsBox"> Evens: { JSON.stringify(this.state.evenArray) } </span>
+        <span className="resultsBox"> Odds: { JSON.stringify(this.state.oddArray) } </span>
+      </div>
+    )
+  }
 }
